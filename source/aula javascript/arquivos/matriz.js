c38e962163b94238ea7833aa4883d45f80fdba69
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2018-04-02 13:51:26 +0000

    Calculadora resolvida matrizes

---
+++ source/aula javascript/arquivos/matriz.js
@@ -0,0 +1,40 @@
+var matriz = [
+    [8, 22, 13, 7],
+    [5, 13, 10, 9],
+    [9, 1, 6, 2],
+    [45, 88, 22, 3]
+];
+
+function mostraMatriz() {
+    for (let i = 0; i < matriz.length; i++) {
+        const linha = matriz[i];
+        var valores = "|";
+        for (let j = 0; j < linha.length; j++) {
+            const coluna = linha[j];
+            if (coluna < 10)
+                valores += "0" + coluna;
+            else
+                valores += coluna;
+            valores += "|";
+        }
+        console.log(valores);
+    }
+}
+
+function somaDiagonalPrincipal() {
+    var total = 0;
+    for (let i = 0; i < matriz.length; i++) {
+        const linha = matriz[i];
+        for (let j = 0; j < linha.length; j++) {
+            const coluna = linha[j];
+            if (i == j) {
+                total += coluna;
+                break;
+            }
+        }
+    }
+    console.log("A soma da diagonal principal é: " + total);
+}
+
+function somaDiagonalSecundaria() {
+}
