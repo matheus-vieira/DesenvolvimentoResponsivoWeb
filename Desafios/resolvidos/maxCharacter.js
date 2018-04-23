d438bda3ce1d56481912b0b7ae5186e46eedac89
Matheus Costa Vieira <matheus.vieira@opet.edu.br>
2018-04-23 14:17:49 -0300

    implementado

--- Desafios/resolvidos/maxCharacter.js
+++ Desafios/resolvidos/maxCharacter.js
@@ -1,3 +1,25 @@
 function maxCharacter(str) {
-    
+    var charMap = {};
+    var maxNum = 0;
+    var maxChar = '';
+
+    //percorre a string
+    for (var i = 0; i < str.length; i++) {
+        //se não contabilizou o caracter inicia com zero
+        if (!charMap[str[i]])
+            charMap[str[i]] = 0;
+
+        // incrementa em um
+        charMap[str[i]]++;
+    }
+
+    // para percorrer propriedades em um objeto
+    // utilizamos o for..in
+    for (var char in charMap) {
+        if (charMap[char] > maxNum) {
+            maxNum = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
 }
