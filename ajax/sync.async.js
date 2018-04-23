5e7a16706389926d6cdc69c50764cbeb35ddad06
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2018-04-23 20:09:46 -0300

    request na mão

--- ajax/sync.async.js
+++ ajax/sync.async.js
@@ -1,23 +1,21 @@
-var id;
-function retornaDados(callback) {
-    var retorno = 1;
-    console.log("executando função retornaDados");
-    id = window.setInterval(function () {
-        console.log("chamando função de callback");
-        callback(retorno++);
-        console.log("finalizando função de callback");
-    },5000);
-    console.log("finalizando função retornaDados");
-}
-
 function local() {
     console.log("executando função local");
     retornaDados(executaRetorno);
     console.log("finalizando função local");
 }
 
-function executaRetorno(retorno) {
+function retornaDados(callback) {
+    var retorno = 1;
+    console.log("executando função retornaDados");
+    window.setTimeout(function () {
+        console.log("chamando função de callback");
+        callback(retorno++);
+        console.log("finalizando função de callback");
+    },0);
+    console.log("finalizando função retornaDados");
+}
+
+function executaRetorno() {
     console.log("executando função executaRetorno");
     console.log("finalizando função executaRetorno");
-    console.log
 }
